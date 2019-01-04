2b08bed5be82e44946d6d2f507997f322be65ec8
libei <18970617@qq.com>
2019-01-04 16:50:23 +0800

    添加前端模板为thymeleaf

--- src/main/java/com/zili/myproject/controller/IndexController.java
+++ src/main/java/com/zili/myproject/controller/IndexController.java
@@ -2,14 +2,15 @@ package com.zili.myproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
-    @RequestMapping(value = "/index",method = RequestMethod.GET)
-    public String index(ModelMap map){
-        map.put("name", "yizhici");
+    @RequestMapping(value = "/index/{name}",method = RequestMethod.GET)
+    public String index(ModelMap map, @PathVariable String name){
+        map.put("name", name);
         return "index";
     }
 }
